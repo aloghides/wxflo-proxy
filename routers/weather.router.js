a1d1f323db24cb8bb999dfe69287a09978b0c6b1
aloghides <6305141+aloghides@users.noreply.github.com>
2020-09-03 22:23:14 -0500

    cleanup requirements

--- routers/weather.router.js
+++ routers/weather.router.js
@@ -8,8 +8,9 @@ axios.defaults.headers.common['User-Agent'] = '(dev weather app, adam.m.loghides
 
 //Mapbox Geolocation
 //const { apiKey, mapboxAPIKey } = require('../credentials');
-const apiKey = process.env.API_KEY || require('../credentials').apiKey;
-const mapboxAPIKey = process.env.MAPBOX_API_KEY || require('../credentials').mapboxAPIKey;
+//const apiKey = process.env.API_KEY || require('../credentials').apiKey;
+const mapboxAPIKey = process.env.MAPBOX_API_KEY 
+//|| require('../credentials').mapboxAPIKey;
 const mbxClient = require('@mapbox/mapbox-sdk');
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const { reset } = require('nodemon');
